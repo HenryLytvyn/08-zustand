9a0d23e88894601b2a789f020b28f0484cc00abb
Henry <hennadii.lytvyn@gmail.com>
2025-07-05 22:04:38 +0100

    pagination edit

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -15,18 +15,16 @@ export default function Pagination({
   onPageChange,
 }: PaginationProps) {
   return (
-    <ul>
-      <ReactPaginate
-        pageCount={totalPages}
-        pageRangeDisplayed={5}
-        marginPagesDisplayed={1}
-        onPageChange={({ selected }) => onPageChange(selected + 1)}
-        forcePage={currentPage - 1}
-        containerClassName={css.pagination}
-        activeClassName={css.active}
-        nextLabel="→"
-        previousLabel="←"
-      />
-    </ul>
+    <ReactPaginate
+      pageCount={totalPages}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }) => onPageChange(selected + 1)}
+      forcePage={currentPage - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
   );
 }
