5419ba00b0ba2d5e69d15a871f0f77a0e6687570
Henry <hennadii.lytvyn@gmail.com>
2025-07-07 22:01:05 +0100

    test 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,9 +60,9 @@ export default function RootLayout({
         <TanStackProvider>
           <Header />
           <main className={css.main}>{children}</main>
-          {modal}
+          {/* {modal} */}
           <Footer />
-          {/* <div style={{ position: 'fixed', top: 0, left: 0 }}>{modal}</div> */}
+          <div style={{ position: 'fixed', top: 0, left: 0 }}>{modal}</div>
         </TanStackProvider>
       </body>
     </html>
